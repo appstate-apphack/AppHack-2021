54b3bdb0b6659b8294e55263fe44fbf8afaa08f2
Jenny Ly <lycb@appstate.edu>
2019-09-27 13:03:37 -0400

    build successful in cs.appstate.edu server

--- src/app/clubs.ts
+++ src/app/clubs.ts
@@ -8,11 +8,11 @@ export const PARTICIPATING_CLUBS: Club[] = [
 	{
 		clubName: "Women in Computer Science",
 		clubDescription: "The Women in Computer Science Club, or WICS, is an organization dedicated to supporting and encouraging women in technical majors, as well as providing opportunities outside the classroom and creating a strong, welcoming community.",
-		logoPath: "../assets/wicslogo.png",
+		logoPath: "apphack/assets/wicslogo.png",
 	},
 	{
 		clubName: "Appalachian Society for Computing, Informatics, and Innovation",
 		clubDescription: "ASCII is an organization dedicated to improving the community for Computer Science students, providing career opportunities, and educating students in diverse concepts across the CS field.",
-		logoPath: "../assets/ah.ico",
+		logoPath: "apphack/assets/asciilogo.jpg",
 	},
 ]
