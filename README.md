d985a10be52fd304fa85abf7978c220fcf9dd95b
Jenny Ly <lycb@appstate.edu>
2020-02-05 09:47:47 -0500

    update latest changes

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Apphack Website
 
 Before you edit. Please go through the Angular tutorial. 
+
+## Contribute
+
+* Clone the project onto your local machine.
+* Install AngularCLI
+* Once done with editing, `ng build --base-href /apphack --deploy-url /apphack/ --prod`
+* `scp` the `dist` folder into `cs.appstate.edu` server 
--- src/app/question.ts
+++ src/app/question.ts
@@ -6,7 +6,7 @@ export class Question {
 export const FAQ: Question[] = [
 	{
 		question: "What is a hackathon?",
-		answer: "A hackathon is a weekend of fun and coding. You will learn how to become a better programmer, meet new faces, and get free food! You will also have the opportunity to win prices and network with our sponsors."
+		answer: "A hackathon is a weekend of fun and coding. You will learn how to become a better programmer, meet new faces, and get free food! You will also have the opportunity to win prizes and network with our sponsors."
 	},
 	{
 		question: "Am I allowed to participate?",
@@ -18,7 +18,7 @@ export const FAQ: Question[] = [
 	},
 	{
 		question: "What about sleep?",
-		answer: "You can totally sleep! Bring your own pillow and sleeping bag, or blankets. There will be a designated sleeping space."
+		answer: "You can totally sleep! Bring your own pillow and sleeping bag, blankets, or air mattress. There will be a designated sleeping space."
 	},
 	{
 		question: "Will there be food?",
