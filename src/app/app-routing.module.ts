a7ed4a33eccedf63d271299d97ba65f496673508
Jenny Ly <lycb@appstate.edu>
2020-09-25 01:28:42 -0400

    update url routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,9 @@ import { FaqComponent } from './faq/faq.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: TitleComponent },
-  //{ path: 'schedule', component: ScheduleComponent },
+  { path: '', component: TitleComponent, pathMatch: 'full' },
+  // { path: 'home', component: TitleComponent },
+  { path: 'schedule', component: ScheduleComponent },
   { path: 'faq', component: FaqComponent },
 ];
 
